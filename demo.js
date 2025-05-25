999e41dcdfae0ff60d2d68dce4d03b5608962176
unknown <bistarabin85@gmail.com>
2025-05-25 08:51:21 +0545

    EVEN NUMBER FILTER FUNCTIONS

--- demo.js
+++ demo.js
@@ -1,1 +1,9 @@
 console.log("REPO FILE TUTORIAL")
+const ar=[1,2,3,4,5]
+const filterEven=()=>{
+    const result= ar.filter((num)=> num%2===0
+        )
+        return result;
+    }
+
+console.log(filterEven());
